3a2f1d09aa7ddaf1c9b216d7aed57c5af0efb192
bluesnake0818 <raidient87@gmail.com>
2022-03-26 01:43:31 +0900

    complete landing page

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,9 +1,13 @@
 import styles from './Landing.module.css'
+import { NavLink } from 'react-router-dom'
 
 const Landing = ({ user }) => {
   return (
     <main className={styles.container}>
-      <h1>hello, {user ? user.name : 'friend'}</h1>
+      <h1>hangfive compatibility test</h1>
+      <h1>What's my relationship style and how compatible am I with my friends?</h1>
+      <img src="/12_Earthly_Branches.png" alt="12 Zodiacs" />
+      <NavLink to='/zodiac'><button className='btn'>Start</button></NavLink>
     </main>
   )
 }
