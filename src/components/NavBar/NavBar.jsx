bcf211c55a3b3f2e155a49de2067cc39d639c5e9
bluesnake0818 <raidient87@gmail.com>
2022-03-27 06:15:57 +0900

    change navlinks' position from left to right

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,14 +4,29 @@ import './NavBar.css'
 const NavBar = ({ user, handleLogout }) => {
   return (
     <div className='navBarMain'>
-      <div className='navLinks'>
+      {/* <div className='navLinks'>
         {user ?
           <nav>
             <ul>
               <li>Welcome, {user.name}</li>
-              <li><Link to="/profiles">Profiles</Link></li>
-              <li><Link to="" onClick={handleLogout}>LOG OUT</Link></li>
               <li><Link to="/changePassword">Change Password</Link></li>
+            </ul>
+          </nav>
+        :
+          <nav>
+
+          </nav>
+        }
+      </div> */}
+      <p className='title'>
+        hangfive
+      </p>
+      <div className='profile'>
+        {user ?
+          <nav>
+            <ul>
+              <li><Link to="/profiles">Profiles</Link></li>
+              <li><Link to="" onClick={handleLogout}>Log Out</Link></li>
             </ul>
           </nav>
         :
@@ -21,9 +36,8 @@ const NavBar = ({ user, handleLogout }) => {
               <li><Link to="/signup">Sign Up</Link></li>
             </ul>
           </nav>
-        }
+        } 
       </div>
-      <p className='title'>hangfive</p>
     </div>
   )
 }
