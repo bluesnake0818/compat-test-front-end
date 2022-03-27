31a4fd804ac25e76a1dec845b97484983ab996f9
bluesnake0818 <raidient87@gmail.com>
2022-03-27 09:58:23 +0900

    navigate to /compat after adding friend

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ const App = () => {
   const handleAddFriend = async newFriendData => {
     const newFriend = await friendService.create(newFriendData)
     setFriends([...friends, newFriend])
-    navigate('/')
+    navigate('/compat')
   }
 
   const handleLogout = () => {
