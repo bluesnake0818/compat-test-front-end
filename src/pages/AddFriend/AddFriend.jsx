f7451dbd543bd6ebe6ef81f130d9fea6b7d411b1
bluesnake0818 <raidient87@gmail.com>
2022-03-28 20:34:51 +0900

    change input type to selectin addfriend

--- src/pages/AddFriend/AddFriend.jsx
+++ src/pages/AddFriend/AddFriend.jsx
@@ -49,7 +49,7 @@ function AddFriend(props) {
 					<label htmlFor="birthYear-input" className="form-label">
 						Friend's Birth Year(required)
 					</label>
-					<input 
+					<select 
 						type="text"
 						className="form-control"
 						id="birthYear-input"
@@ -57,7 +57,12 @@ function AddFriend(props) {
             value={formData.birthYear}
             onChange={handleChange}
 						required
-					/>
+					>
+						<option value="1980">1980</option>
+						<option value="1981">1981</option>
+						<option value="1982">1982</option>
+						<option value="1983">1983</option>
+					</select>
 				</div>
 				<div className="d-grid">
 					<button
@@ -74,3 +79,11 @@ function AddFriend(props) {
 }
 
 export default AddFriend
+
+{/* <label for="mpaa-rating-select">MPAA Rating</label>
+<select name="mpaaRating" id="mpaa-rating-select">
+	<option value="G">G</option>
+	<option value="PG">PG</option>
+	<option value="PG-13">PG-13</option>
+	<option value="R">R</option>
+</select> */}
