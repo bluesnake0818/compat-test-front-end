61110602fa87ecaf603ff7a54448a8de0a893e55
bluesnake0818 <raidient87@gmail.com>
2022-03-30 07:45:06 +0900

    restyle friend card in compat

--- src/components/FriendCard/FriendCard.jsx
+++ src/components/FriendCard/FriendCard.jsx
@@ -1,38 +1,88 @@
 import { Link } from 'react-router-dom'
 import './FriendCard.css'
 import ShowCompat from '../../pages/ShowCompat/ShowCompat'
+import Box from '@mui/material/Box'
+import Paper from '@mui/material/Paper'
+import ListItem from '@mui/material/ListItem'
+import List from '@mui/material/List'
+import Divider from '@mui/material/Divider'
+import ListItemText from '@mui/material/ListItemText'
+import ListItemIcon from '@mui/material/ListItemIcon'
+import PersonIcon from '@mui/icons-material/Person'
+import CakeIcon from '@mui/icons-material/Cake'
+import PetsIcon from '@mui/icons-material/Pets'
 
 
 function FriendCard({friend, handleDeleteFriend, user}) {
-  
-  return(
-    <div className="card">
-      <div className="card-body">
-        <Link
-          to='/showCompat' 
-          state={{friend}}
-        >
-          <h2 className="card-text">{friend.name} {friend.birthYear} {friend.zodiac}</h2>
-        </Link>
-      </div>
-      <div className="card-footer">
-        <Link
-          to='/edit'
-          state={{friend}}
-        >
-          <button className='btn btn-sm btn-warning'>
-            Edit
-          </button>
-        </Link>
-        <button
-          className="btn btn-sm btn-danger m-left"
-          onClick={()=> handleDeleteFriend(friend._id)}
-        >
-          Delete
-        </button>
-      </div>
-    </div>
-  )
+  return ( 
+    <Box
+    display="flex" 
+    justifyContent="center" 
+    alignItems="center"
+    sx={{ width: "100%" }}
+    >
+      <Paper sx={{ width: '100%' }}>
+        <List>
+          <ListItem>
+            <ListItemIcon>
+              <PersonIcon />
+            </ListItemIcon>
+              <Link
+                to='/showCompat' 
+                state={{friend}}
+              >
+                <ListItemText primary={friend.name} secondary="Name"/>
+              </Link>
+          </ListItem>
+          <Divider />
+          <ListItem>
+            <ListItemIcon>
+              <CakeIcon />
+            </ListItemIcon>
+            <ListItemText primary={friend.birthYear} secondary="Birth Year" />
+          </ListItem>
+          <Divider />
+          <ListItem>
+            <ListItemIcon>
+              <PetsIcon />
+            </ListItemIcon>
+            <ListItemText primary={friend.zodiac} secondary="Zodiac" />
+          </ListItem>
+        </List>
+      </Paper>
+    </Box>
+  );
+
 }
 
 export default FriendCard
+
+// return(
+//   <div className="card">
+//     <div className="card-body">
+//       <Link
+//         to='/showCompat' 
+//         state={{friend}}
+//       >
+//         <h2 className="card-text">{friend.name} {friend.birthYear} {friend.zodiac}</h2>
+//       </Link>
+//     </div>
+//     <div className="card-footer">
+//       <Link
+//         to='/edit'
+//         state={{friend}}
+//       >
+//         <button className='btn btn-sm btn-warning'>
+//           Edit
+//         </button>
+//       </Link>
+//       <button
+//         className="btn btn-sm btn-danger m-left"
+//         onClick={()=> handleDeleteFriend(friend._id)}
+//       >
+//         Delete
+//       </button>
+//     </div>
+//   </div>
+// )
+
--- src/pages/ShowCompat/ShowCompat.jsx
+++ src/pages/ShowCompat/ShowCompat.jsx
@@ -3,7 +3,6 @@ import { Link, useLocation } from 'react-router-dom'
 import './ShowCompat.css'
 import CompatView from '../../components/CompatView/CompatView';
 import { CompatData } from '../../components/CompatView/CompatData';
-import * as profileService from '../../services/profileService'
 import reactDom from "react-dom";
 import React from "react";
 
@@ -15,13 +14,10 @@ function ShowCompat({ user }) {
   const friend = location.state.friend
   const yourZod = friend.owner.zodiac
   const theirZod = friend.zodiac
-  // const compatObj = compats.find(compat => compat.id === yourZod)
-  // const compatArr = compatObj.compats
-  // const compatFriend = compatArr.find(compatFr => compatFr.id === theirZod)
 
   return (
     <div className='showCompat'>
-      {/* <CompatView CompatData = {CompatData} /> */}
+
       <div>
         {friend.owner.name}'s zodiac is {yourZod}.
       </div>
@@ -38,14 +34,3 @@ function ShowCompat({ user }) {
 export default ShowCompat;
 
 
-{/* <div> */}
-  {/* {compatFriend.compats} */}
-{/* {compats.yourZod.theirZod} */}
-{/* </div>
-
-
-
-
-{/* {compats.map(compat => 
-  <p key={compat.id}>You are a {yourZod}. Your compatibility with a {theirZod} is: {compat.id}</p>
-)} */}
