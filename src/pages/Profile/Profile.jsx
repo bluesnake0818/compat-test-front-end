d0430d0086b3b0f3fad2dde65e0fbaefda2f0fd3
bluesnake0818 <raidient87@gmail.com>
2022-03-30 09:20:38 +0900

    create profiledata

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,28 +1,13 @@
 
-import { Link } from 'react-router-dom';
-import FriendCard from '../../components/FriendCard/FriendCard';
+
+import { ProfileData } from './ProfileData';
 
 const Profile = (props) => {
   return (
       <div className='main'>
         <h3>{props.user.name}'s Profile</h3>
-        <div className='friend-list'>
-          {props.friends.length ?
-              <ul>
-                  {props.friends.map(friend => (
-                    <li>
-                    <FriendCard
-                      key={friend._id} 
-                      friend={friend} 
-                      user={props.user}
-                    />
-                    </li>
-                  ))}   
-              </ul>
-          :
-          <p>You have no friends yet.</p> 
-        }
-        </div>
+        {/* <p>Your zodiac is {props.friends[0].owner.zodiac}</p> */}
+
       </div>
   )
 }
