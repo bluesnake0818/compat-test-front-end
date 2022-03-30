b3eb93653e924f328e85a701828f8b95b359a5fb
bluesnake0818 <raidient87@gmail.com>
2022-03-30 14:52:28 +0900

    update useSate and useEffect to access profile

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,13 +1,49 @@
 import { ProfileData } from './ProfileData';
-
+import { useState, useEffect } from 'react'
+import * as profileService from '../../services/profileService'
 
 const Profile = (props) => {
-  return (
-      <div className='main'>
-        <h3>{props.user.name}'s Profile</h3>
-        {/* <p>Your zodiac is {props.friends[0].owner.zodiac}</p> */}
+  // const [profiles, setProfiles] = useState([])
+  const [profile, setProfile] = useState({
+    name: ''
+    }
+  )
 
-      </div>
+  useEffect(()=> {
+    profileService.getAllProfiles()
+    .then(profiles =>
+      setProfile(profiles.find(profile => profile.id === props.user.profile.id))
+      // .then(profile => setProfile(profile))
+    )
+  }, [])
+
+  // useEffect(()=> {
+  //   profiles.find(profile => profile.id === props.user.profile.id)
+  //   .then(profile => setProfile(profile))
+  // }, [])
+
+  // useEffect(()=> {
+  //   setProfile(profiles.find(profile => profile.id === props.user.profile.id))
+  // }, [])
+
+  // const userProfile = profiles.find(profile => profile.id === props.user.profile.id)
+  // console.log(userProfile.name)
+
+  // console.log(profiles)
+
+  return (
+    <div className='main'>
+      <h3>{props.user.name}'s Profile</h3>
+        <ul>
+          <li>
+            {profile.zodiac}
+          </li>
+          <li>
+            {ProfileData[0].id}
+          </li>
+        </ul>
+
+    </div>
   )
 }
 
