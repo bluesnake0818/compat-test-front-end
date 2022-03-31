681e0868ea22f7270da4fdd48ed2fafdc000d56c
bluesnake0818 <raidient87@gmail.com>
2022-04-01 02:17:56 +0900

    add arrows back and restyle /signupOrLogin

--- src/pages/SignupOrLogin/SignupOrLogin.jsx
+++ src/pages/SignupOrLogin/SignupOrLogin.jsx
@@ -7,8 +7,8 @@ const SignupOrLogin = props => {
     <main className={styles.container}>
       <h3 className={styles.title}>Sign up to access zodiac analysis & compatibility reports for free!</h3>
       <div className={styles.links}>
-        <Link to="/signup"><button className='button'>Signup</button></Link>
-        <Link to="/login"><button className='button'>Login</button></Link>
+        <Link to="/signup"><button className={styles.button}>Signup</button></Link>
+        <Link to="/login"><button className={styles.button}>Login</button></Link>
       </div>
       <p className={styles.instructions}>If you are a returning user, please log in.</p>
     </main>
