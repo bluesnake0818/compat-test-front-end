68947bb0b6978b3548393b7cb3ed7ccf808a8969
bluesnake0818 <raidient87@gmail.com>
2022-04-01 00:01:47 +0900

    test 3

--- src/App.jsx
+++ src/App.jsx
@@ -74,99 +74,99 @@ const App = () => {
   //   setUser(authService.getUser())
   // }
   
-  return (
-    <main className={styles.container}>
-      <NavBar user={user} handleLogout={handleLogout} />
-      <Routes>
-        <Route 
-          path="/" 
-          element={<Landing />} 
-        />
-        <Route
-          path="/zodiac"
-          element={<Zodiac />}
-        />
-        <Route
-          path="/signupOrLogin"
-          element={<SignupOrLogin />}
-        />
-        <Route
-          path="/signup"
-          element={<Signup handleSignupOrLogin={handleSignupOrLogin} />}
-        />
-        <Route
-          path="/login"
-          element={<Login handleSignupOrLogin={handleSignupOrLogin} />}
-        />
-        <Route
-          path="/changePassword"
-          element={user ? <ChangePassword handleSignupOrLogin={handleSignupOrLogin}/> : <Navigate to="/login" />}
-        />
-        <Route
-          path="/PremiumContent"
-          element={
-            user ?
-            <PremiumContent 
-              user={user}
-              profile={profile}
-            />
-            : <Navigate to="/signupOrLogin"  />
-          }
-        />
-        <Route
-          path="/compat"
-          element={
-            user ?
-            <Compat 
-              handleDeleteFriend={handleDeleteFriend}
-              friends={friends}
-              user={user}
-            />
-            : <Navigate to="/signupOrLogin" />
-          }
-        />
-        <Route
-          path="/showCompat"
-          element={
-            <ShowCompat 
-              // handleDeleteFriend={handleDeleteFriend}
-              friends={friends}
-              user={user}
-            />
-          }
-        />
-        <Route
-          path="/addFriend"
-          element={<AddFriend handleAddFriend={handleAddFriend} />}
-        />
-        <Route 
-          path='/edit'
-          element={
-            <EditFriend
-              handleUpdateFriend={handleUpdateFriend}
-            />
-          }
-        />
-        <Route
-          path="/profiles"
-          element={user ? <Profiles user={user} /> : <Navigate to="/login" />}
-        />
-        <Route
-          path="/profile"
-          element={user ?
-            <Profile 
-              friends={friends}
-              user={user}
-              profile={profile}
-              handleLogout={handleLogout}
-            />
-            : 
-            <Navigate to="/login" />
-          }
-        />
-      </Routes>
-    </main>
-  )
+//   return (
+//     <main className={styles.container}>
+//       <NavBar user={user} handleLogout={handleLogout} />
+//       <Routes>
+//         <Route 
+//           path="/" 
+//           element={<Landing />} 
+//         />
+//         <Route
+//           path="/zodiac"
+//           element={<Zodiac />}
+//         />
+//         <Route
+//           path="/signupOrLogin"
+//           element={<SignupOrLogin />}
+//         />
+//         <Route
+//           path="/signup"
+//           element={<Signup handleSignupOrLogin={handleSignupOrLogin} />}
+//         />
+//         <Route
+//           path="/login"
+//           element={<Login handleSignupOrLogin={handleSignupOrLogin} />}
+//         />
+//         <Route
+//           path="/changePassword"
+//           element={user ? <ChangePassword handleSignupOrLogin={handleSignupOrLogin}/> : <Navigate to="/login" />}
+//         />
+//         <Route
+//           path="/PremiumContent"
+//           element={
+//             user ?
+//             <PremiumContent 
+//               user={user}
+//               profile={profile}
+//             />
+//             : <Navigate to="/signupOrLogin"  />
+//           }
+//         />
+//         <Route
+//           path="/compat"
+//           element={
+//             user ?
+//             <Compat 
+//               handleDeleteFriend={handleDeleteFriend}
+//               friends={friends}
+//               user={user}
+//             />
+//             : <Navigate to="/signupOrLogin" />
+//           }
+//         />
+//         <Route
+//           path="/showCompat"
+//           element={
+//             <ShowCompat 
+//               // handleDeleteFriend={handleDeleteFriend}
+//               friends={friends}
+//               user={user}
+//             />
+//           }
+//         />
+//         <Route
+//           path="/addFriend"
+//           element={<AddFriend handleAddFriend={handleAddFriend} />}
+//         />
+//         <Route 
+//           path='/edit'
+//           element={
+//             <EditFriend
+//               handleUpdateFriend={handleUpdateFriend}
+//             />
+//           }
+//         />
+//         <Route
+//           path="/profiles"
+//           element={user ? <Profiles user={user} /> : <Navigate to="/login" />}
+//         />
+//         <Route
+//           path="/profile"
+//           element={user ?
+//             <Profile 
+//               friends={friends}
+//               user={user}
+//               profile={profile}
+//               handleLogout={handleLogout}
+//             />
+//             : 
+//             <Navigate to="/login" />
+//           }
+//         />
+//       </Routes>
+//     </main>
+//   )
 }
 
 export default App
