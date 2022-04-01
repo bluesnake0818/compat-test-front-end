e60e2462354db9b086f0476f363c4a4006acb183
bluesnake0818 <raidient87@gmail.com>
2022-04-01 10:29:28 +0900

    change login button to link with underline on /SingupOrLogin

--- src/pages/SignupOrLogin/SignupOrLogin.jsx
+++ src/pages/SignupOrLogin/SignupOrLogin.jsx
@@ -8,9 +8,11 @@ const SignupOrLogin = props => {
       <h3 className={styles.title}>Sign up to access zodiac analysis & compatibility reports for free!</h3>
       <div className={styles.links}>
         <Link to="/signup"><button className={styles.button}>Signup</button></Link>
-        <Link to="/login"><button className={styles.button}>Login</button></Link>
+        {/* <Link to="/login"><button className={styles.button}>Login</button></Link> */}
+        <Link to="/login">
+          <p className={styles.instructions}>If you are a returning user, please log in.</p>
+        </Link>
       </div>
-      <p className={styles.instructions}>If you are a returning user, please log in.</p>
     </main>
   )
 }
