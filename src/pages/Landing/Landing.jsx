f668934d0cccb9c512aa5ad4d7611fbb619d98a9
bluesnake0818 <raidient87@gmail.com>
2022-04-01 21:19:17 +0900

    font adjustment

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Landing = () => {
   return (
     <main className={styles.container}>
-      <h1 className={styles.title}>Zodiac Sign & Compatibility Test</h1>
+      <h2 className={styles.title}>Zodiac Sign & Compatibility Test</h2>
       <img src="/12_Earthly_Branches.png" alt="12 Zodiacs" />
       <Link to='/zodiac'><button className={styles.button}>Start</button></Link>
     </main>
